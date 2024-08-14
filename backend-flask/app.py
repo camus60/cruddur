e9fb8277e3ae921a974d8a84a913073e2809317e
Camus <mkambona@gmail.com>
2024-08-14 15:11:39 +0300

    FEAT:conditionally show components based on loggedin or logged out

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -25,9 +25,22 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
-# X-RAY ----------
-# xray_url = os.getenv("AWS_XRAY_URL")
-# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+
+# cloudwatch logging
+import watchtower
+import logging
+from time import strftime
+
+#Configure cloudwatch logging
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some message")
+
 
 ## Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
@@ -68,11 +81,12 @@ cors = CORS(
     methods="OPTIONS,GET,HEAD,POST",
 )
 
-# @app.after_request
-# def after_request(response):
-#     timestamp = strftime('[%Y-%b-%d %H:%M]')
-#     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-#     return response
+# Cloudwatch error logging code
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 
 @app.route("/api/message_groups", methods=["GET"])
